d1fdaada1bed0b31068c30e6e03149069d348404
priscu vlad <vlad.priscu@globant.com>
2026-07-15 16:55:11 +0300

    Mvovie-app base code

---
+++ movie-app/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+</script>
+
+<template>
+<div class="min-h-screen bg-black text-white font-sans">
+<header class="flex justify-between items-center p-4 bg-linear-to-b from-black via-gray-900 to-transparent">
+  <nav class="flex gap-6">
+    <button class="text-sm bg-red-600 px-4 py-1 rounded hover:bg-red-700">Login</button>
+  </nav>
+</header>
+<main class="p-4">
+</main>
+
+<footer class="text-center text-gray-500 text-sm p-4 border-t border-gray-700">
+  © 2025 MovieApp.
+</footer>
+</div>
+</template>
