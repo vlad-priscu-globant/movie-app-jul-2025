73aa3e1933546e5c681548ee4b081d298060a087
Denis Andrei <denisjircan@gmail.com>
2026-08-12 15:02:29 +0300

    Add SEO meta and OG tags to movie detail page

--- pages/movie/[id].vue
+++ pages/movie/[id].vue
@@ -12,6 +12,14 @@ const { data: movie, pending, error } = await useFetch<MovieDetail>(`/api/movies
 const { isFavorite, toggleFavorite } = useFavorites()
 const user = useSupabaseUser()
 
+useSeoMeta({
+  title: () => movie.value ? `${movie.value.title} - Premium Movie DB` : 'Premium Movie DB',
+  ogTitle: () => movie.value?.title,
+  ogDescription: () => movie.value?.overview,
+  ogImage: () => movie.value?.image,
+  ogType: 'video.movie'
+})
+
 const onToggleFavorite = () => {
   if (!user.value) {
     alert('Trebuie să fii autentificat pentru a adăuga la favorite.')
